d1f3fda3e7b845c2b795da6065ade17875493ac3
heman chandra <hemanchandra701@gmail.com>
2026-03-14 18:43:01 +0530

    terraform

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.34.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "remote-state-chandra"
+    key    = "roboshop-dev-vpc"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
---
+++ 10-sg/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "sg_name" {
+    default = [
+        "mongodb", "redis", "mysql", "rabbitmq",
+        "catalogue", "user", "cart", "shipping", "payment",
+        "frontend",
+        "bastion"
+    ]
+}
