bd143edae04ea96d1cb78e8e1cdb94c252bf5807
heman chandra <hemanchandra701@gmail.com>
2026-03-21 15:04:27 +0530

    terraform

--- 50-backend-alb/provider.tf
+++ 50-backend-alb/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.34.0"
+      version = "6.37.0"
     }
   }
 
   backend "s3" {
     bucket = "remote-state-chandra"
-    key    = "roboshop-dev-vpc"
+    key    = "roboshop-dev-backend-alb"
     region = "us-east-1"
   }
 }
