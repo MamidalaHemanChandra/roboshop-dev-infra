b2331c744d8f9c10974780bac9e582dbeea86b55
heman chandra <hemanchandra701@gmail.com>
2026-03-25 11:11:11 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -15,3 +15,30 @@ resource "aws_security_group_rule" "bastion_laptop" {
   security_group_id = local.bastion_sg_id
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.bastion_sg_id
+}
+
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.redis_sg_id
+  source_security_group_id = local.bastion_sg_id
+}
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.rabbitmq_sg_id
+  source_security_group_id = local.bastion_sg_id
+}
