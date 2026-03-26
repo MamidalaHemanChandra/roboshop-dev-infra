8ac3c909483aa4acf02b0a83e6e5012e44ee3d4e
heman chandra <hemanchandra701@gmail.com>
2026-03-26 13:16:46 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -48,6 +48,6 @@ resource "aws_security_group_rule" "mysql_bastion" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  security_group_id = local.rabbitmq_sg_id
+  security_group_id = local.mysql_sg_id
   source_security_group_id = local.bastion_sg_id
 }
