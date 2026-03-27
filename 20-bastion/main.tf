0e626a38b2acc6beb798662327e32ca9a5cc8096
heman chandra <hemanchandra701@gmail.com>
2026-03-27 18:32:26 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
     instance_type           = "t3.micro"
     vpc_security_group_ids  = [local.bastion_sg_id]
     subnet_id = local.subnet_id
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     user_data = file("bastion.sh")
 
     tags = merge(
@@ -11,4 +12,34 @@ resource "aws_instance" "bastion" {
       Name = "${local.common_name}-bastion"
     }
   )
+}
+
+#Create IAM Role for bastion
+resource "aws_iam_role" "bastion" {
+  name = "bastion"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+#Attach AdministratorAccess policy
+resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# 3. Create Instance Profile
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = aws_iam_role.bastion.name
 }
