220bf60ee4fd4b7f57b8d0d35904ea3dd5150b9a
heman chandra <hemanchandra701@gmail.com>
2026-03-29 17:25:49 +0530

    terraform

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "s3" {
     bucket = "remote-state-chandra"
-    key    = "roboshop-dev-catalogue"
+    key    = "roboshop-dev-cat"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
