3dfd47574172c1943bd988d49d21e47abde07c0a
heman chandra <hemanchandra701@gmail.com>
2026-04-01 23:21:23 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -1,27 +1,22 @@
-variable component {
-  default     = {
-    catalogue {
-        priority = 10
+variable "component" {
+  default = {
+    catalogue = {
+      priority = 10
     }
-    user {
-        priority = 20
+    user = {
+      priority = 20
     }
-    cart {
-        priority = 30
+    cart = {
+      priority = 30
     }
-    shipping {
-        priority = 40
+    shipping = {
+      priority = 40
     }
-    payment {
-        priority = 50
+    payment = {
+      priority = 50
     }
-    frontend {
-       priority = 10 
+    frontend = {
+      priority = 10
     }
-
   }
-}
-
-variable priority {
-
 }
